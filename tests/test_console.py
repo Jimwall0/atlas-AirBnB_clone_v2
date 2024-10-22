354b7524fb1cc0008ee46bf92a02b2257957d948
namestesensei <christopherwright435@gmail.com>
2024-10-21 20:31:50 -0500

    Created unit tests for console and models; fixed pycodestyle issues

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest for console
+"""
+import unittest
+from console import HBNBCommand
+
+class TestConsole(unittest.TestCase):
+    """Test the console"""
+
+    def test_do_create(self):
+        """Test the create command"""
+        pass
+
+if __name__ == "__main__":
+    unittest.main(i)

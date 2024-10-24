600dc61f919ae7890ec569ac3392fa1a96dfd50a
namestesensei <christopherwright435@gmail.com>
2024-10-23 20:46:22 -0500

    updted user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,30 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+class BaseModel:
+    @classmethod
+    def create(cls, **kwargs):
+        raise NotImplementedError("Must be implemented in child classes.")
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    @classmethod
+    def get_by_id(cls, id):
+        raise NotImplementedError("Must be implemented in child classes.")
+
+    @classmethod
+    def update(cls, id, **kwargs):
+        raise NotImplementedError("Must be implemented in child classes.")
+
+    @classmethod
+    def delete(cls, id):
+        raise NotImplementedError("Must be implemented in child classes.")
+
+class User(BaseModel, SQLAlchemy):
+    __tablename__ = 'users'
+
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(128), default=None, nullable=True)
+    last_name = db.Column(db.String(128), default=None, nullable=True)
+
+    def __repr__(self):
+        return f"User('{self.email}', '{self.first_name}', '{self.last_name}')"

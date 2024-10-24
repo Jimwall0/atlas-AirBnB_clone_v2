a8ae49268a9f05eca33bb87a6d0770d55915a59f
namestesensei <christopherwright435@gmail.com>
2024-10-23 23:59:55 -0500

    Add DBStorage relationships for User, Place, Review, and Amenity models

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This module defines a class Amenity"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
+class Amenity(BaseModel, Base):
+    """This class defines an amenity"""
+    __tablename__ = 'amenities'
 
-class Amenity(BaseModel):
-    name = ""
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
--- models/user.py
+++ models/user.py
@@ -1,30 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-class BaseModel:
-    @classmethod
-    def create(cls, **kwargs):
-        raise NotImplementedError("Must be implemented in child classes.")
 
-    @classmethod
-    def get_by_id(cls, id):
-        raise NotImplementedError("Must be implemented in child classes.")
-
-    @classmethod
-    def update(cls, id, **kwargs):
-        raise NotImplementedError("Must be implemented in child classes.")
-
-    @classmethod
-    def delete(cls, id):
-        raise NotImplementedError("Must be implemented in child classes.")
-
-class User(BaseModel, SQLAlchemy):
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    email = db.Column(db.String(128), nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    first_name = db.Column(db.String(128), default=None, nullable=True)
-    last_name = db.Column(db.String(128), default=None, nullable=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    def __repr__(self):
-        return f"User('{self.email}', '{self.first_name}', '{self.last_name}')"
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")

79cc52b9e977f0625b8d1abfacd3ff610bc48c3e
Tsimmuaj <tyang4204@gmail.com>
2024-11-04 16:13:29 -0600

    more documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
-    """ This is a simple program to print "Hello HBNB!" on a webpage"""
+    """This is a simple program to print "Hello HBNB!" on a webpage"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

f9885a727a1de6f54b580f84801e8331a19b2e29
Tsimmuaj <tyang4204@gmail.com>
2024-11-04 16:23:49 -0600

    task 1 output

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,16 +4,21 @@
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
+/hbnb: display “HBNB”
 You must use the option strict_slashes=False in your route definition"""
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route("/", strict_slashes=False)
 def index():
-    """This is a simple program to print "Hello HBNB!" on a webpage"""
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def index2():
+    return "HBNB"
 
 
 if __name__ == "__main__":

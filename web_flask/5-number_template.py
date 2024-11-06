c99d3fc5c9a92d215498647afe9edc8b0f11e8d3
Tsimmuaj <tyang4204@gmail.com>
2024-11-05 22:53:46 -0600

    formating

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,5 +39,7 @@ def index5(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def index6(n):
     return render_template("5-number.html", n=n)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
